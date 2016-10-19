83d7ce498af4db3131f71d7ac52bd9f9938164cf
zhaoqian3355 <zhaoqian3355@163.com>
2016-10-19 08:17:13 +0800

    print Parent

--- Exercises/dealingWithDescendants.py
+++ Exercises/dealingWithDescendants.py
@@ -4,11 +4,12 @@ from bs4 import BeautifulSoup;
 html=urlopen("http://www.pythonscraping.com/pages/page3.html");
 bsObj= BeautifulSoup(html);
 
-
 def PrintChild():
-    
+    for child in bsObj.find("table",{"id":"giftList"}).children:
+        print(child);
+
+def PrintParent():
+    print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text());
 
 
-
-for child in bsObj.find("table",{"id":"giftList"}).children:
-    print(child);
+PrintParent();
