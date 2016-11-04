060d1e4076fdc72e2911e17c00d88a8b397f29c7
zhao qian <qian.zhao@network.innocellence.com>
2016-11-04 17:07:08 +0800

    create ctripspider

---
+++ ctrip/ctripspider.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import json
+
+fightUrl='http://flights.ctrip.com/domesticsearch/search/SearchFirstRouteFlights?DCity1=DLC&ACity1=SHA&SearchType=S&DDate1=2016-11-05&IsNearAirportRecommond=0&rk=0.8936676051200609145454&CK=052284128A811F5580F548DF4AE52C1E&r=0.53111172658318670744115'
+
+response=urlopen(fightUrl)
+byteData=response.read()
+stringData=byteData.decode('gb2312')
+x=json.loads(stringData)
