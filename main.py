f86fe484d7ac0238d8c142466fcf02420c772049
zhaoqian3355 <zhaoqian3355@163.com>
2016-12-06 23:28:43 +0800

    crawl the wikipedia

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import json
 import re
+pages=set()
+def getLinks(pageUrl):
+    global pages
+    html=urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj=BeautifulSoup(html)
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id="mw-content-text").findall("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("This page is missing something! No worries though!")
 
-html=urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj=BeautifulSoup(html)
- 
-for link in bsObj.find("div",{"id":"bodyContent"}).findAll("a",
-                        href=re.compile("^(/wiki/)((?!:).)*")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+    for link in bsObj.findAll("a",href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs["href"] not in pages:
+                newPage=link.attrs['href']
+                print("-------------\n"+newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+getLinks('')
+
