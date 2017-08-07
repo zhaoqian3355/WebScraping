7439e3702b632051794955d7e13beee02d35027f
zhaoqian3355 <zhaoqian3355@163.com>
2017-08-07 23:53:44 +0800

    modify the train.py

--- train/train.py
+++ train/train.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from urllib.request import urlopen
 from datetime import date,datetime
+from pymongo import MongoClient
 
-today=str(date.today())
+today=str(datetime(2017,6,8))
+# mongoClient=MongoClient("http://localhost:27017")
+# trainData=mongoClient.TrainData
+# train=trainData.Train
 
 def getTrainInfo(date):
     df = pd.read_json("train_list.json")
@@ -27,7 +31,7 @@ def getTrainInfo(date):
     return result
 
 
-dfTrain = getTrainInfo()
+dfTrain = getTrainInfo(today)
 
 
 def getStationInfo():
@@ -59,8 +63,8 @@ result=result.rename(columns={'station_name':'to_station_name'})
 result=result.loc[:,['station_train_code', 'train_no', 'train_type', 'train_code',
         'from_station','from_station_name', 'to_station','to_station_name',
         'from_station_telecode','to_station_telecode']]
-result.to_excel("search.xlsx")
+# result.to_excel("search.xlsx")
 
-def getTrainStation(train_no,from_station,to_station,date):
+# def getTrainStation(train_no,from_station,to_station,date):
     # https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=240000K6810U&from_station_telecode=BJP&to_station_telecode=DLT&depart_date=2017-08-06
-    data=urlopen("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no={0}&from_station_telecode={1}&to_station_telecode={2}&depart_date={3}".format(train_no,from_station,to_station,str(date))
+    # data=urlopen("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no={0}&from_station_telecode={1}&to_station_telecode={2}&depart_date={3}".format(train_no,from_station,to_station,str(date))
