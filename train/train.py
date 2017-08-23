e8d2c2ecf5f3efb260aba186e2e5d3c853e60ddb
ZHAOQIAN3355\Zhao.Qian <qian.zhao@innocellence.com>
2017-08-23 14:26:16 +0800

    add train infos

--- train/train.py
+++ train/train.py
@@ -5,15 +5,18 @@ from pymongo import MongoClient
 import json
 from urllib.request import Request
 import ssl
+from sqlalchemy import create_engine
+import numpy as np
 
-today = str(datetime(2017, 6, 8))
+today = str(datetime(2017, 8, 23))
 mongoClient = MongoClient("mongodb://localhost:27017")
 trainData = mongoClient.TrainData
 train = trainData.Train
 
+engine = create_engine('mssql+pymssql://sa:!QAZxsw2@zhaoqian3355:1433/Ctrip')
 
 def getTrainInfo(date):
-    df = pd.read_json("train_list.json")
+    df = pd.read_json("train_list.js",encoding="utf-8")
     df1 = df[str(date)]
     dfList = []
     for item in df1.index:
@@ -36,7 +39,7 @@ def getTrainInfo(date):
 
 
 def getStationInfo():
-    with open("station_name.txt") as f:
+    with open("station_name.txt",encoding="utf-8") as f:
         stations = f.read()
     stations = stations[:-2]
     stations = stations.split('@')
@@ -65,17 +68,20 @@ def getStationInfo():
 # result = result.loc[:, ['station_train_code', 'train_no', 'train_type', 'train_code',
 #                         'from_station', 'from_station_name', 'to_station', 'to_station_name',
 #                         'from_station_telecode', 'to_station_telecode']]
-# result.to_excel("search.xlsx")
+# result.index=np.arange(1,len(result)+1)
+# result.index.name="Id"
+# result["IsSelect"]=0
+# result.to_excel("train.xlsx")
 
-# df = pd.read_excel("./train/search.xlsx")
+df = pd.read_excel("./train.xlsx")
 
 
 def getTrainStation(train_no, from_station, to_station, date):
     try:
-        context=ssl._create_unverified_context()
-        req=Request("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no={0}&from_station_telecode={1}&to_station_telecode={2}&depart_date={3}".format(
+        context = ssl._create_unverified_context()
+        req = Request("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no={0}&from_station_telecode={1}&to_station_telecode={2}&depart_date={3}".format(
             '11000C100102', 'CCT', 'YXL', '2017-08-10'))
-        data = urlopen(req,context=context)
+        data = urlopen(req, context=context)
         data = json.loads(data.read().decode())
         print(data)
     except Exception as ex:
@@ -89,4 +95,4 @@ def getTrainStation(train_no, from_station, to_station, date):
 #     print(row.train_no)
 #     getTrainStation(row.train_no,row.from_station_telecode,row.to_station_telecode,today)
 
-
+df.to_sql("Train", engine, if_exists="replace",index=False)
